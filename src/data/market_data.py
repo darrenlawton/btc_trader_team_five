5f31333d74a326db5262897f548f2fc2dbb610c5
Darren Lawton <dalawton@deloitte.com.au>
2023-01-26 21:47:54 +1100

    Small update to market data func.

--- src/data/market_data.py
+++ src/data/market_data.py
@@ -15,9 +15,9 @@ def get_price_history_yahoo(ticker, start_date, end_date, interval, store_sql = 
     if not historical_prices.empty and store_sql:
         # Convert column names to SQL suitable convention
         historical_prices = historical_prices.add_prefix('mkt_')
-        conn.create_table("_".join([ticker.replace("-","_"), start_date.strftime(OUT_DATE_FORMAT), end_date.strftime(OUT_DATE_FORMAT)]), historical_prices)
+        store_sql.create_table("_".join([ticker.replace("-","_"), start_date.strftime(OUT_DATE_FORMAT), end_date.strftime(OUT_DATE_FORMAT)]), historical_prices)
     
     return historical_prices
 
-conn = sql.SqlDb("tcp:AUCLD04018656,1433","team_five_aiml_group")
-get_price_history_yahoo("BTC-AUD", datetime.datetime(2020,2,1), datetime.datetime(2023,2,1), "1d", conn)
+#conn = sql.SqlDb("tcp:AUCLD04018656,1433","team_five_aiml_group")
+#get_price_history_yahoo("BTC-AUD", datetime.datetime(2020,2,1), datetime.datetime(2023,2,1), "1d", conn)
