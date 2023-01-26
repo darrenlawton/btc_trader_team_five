dadc71359ea9e7f6bb770ffb127b1ab284998e71
Darren Lawton <dalawton@deloitte.com.au>
2023-01-26 21:40:10 +1100

    Market data function.

--- src/data/market_data.py
+++ src/data/market_data.py
@@ -1,1 +1,23 @@
-def get_price_history_yahooc():
+# Libraries to import
+import pandas as pd
+import yfinance as yf
+import datetime
+import sql_functions as sql
+
+# Global variables
+OUT_DATE_FORMAT = '%Y%m%d'
+
+
+def get_price_history_yahoo(ticker, start_date, end_date, interval, store_sql = None):
+    historical_prices = yf.download(ticker, start=start_date, end=end_date, interval=interval)
+    historical_prices.reset_index(inplace=True)
+
+    if not historical_prices.empty and store_sql:
+        # Convert column names to SQL suitable convention
+        historical_prices = historical_prices.add_prefix('mkt_')
+        conn.create_table("_".join([ticker.replace("-","_"), start_date.strftime(OUT_DATE_FORMAT), end_date.strftime(OUT_DATE_FORMAT)]), historical_prices)
+    
+    return historical_prices
+
+conn = sql.SqlDb("tcp:AUCLD04018656,1433","team_five_aiml_group")
+get_price_history_yahoo("BTC-AUD", datetime.datetime(2020,2,1), datetime.datetime(2023,2,1), "1d", conn)
--- src/data/sql_functions.py
+++ src/data/sql_functions.py
@@ -1,4 +1,23 @@
+# Libraries to import
 import pyodbc
+
+
+# Global variables
+DATA_TYPE_MAPPING = {
+        'int64'     : 'int'
+    ,   'float64'   : 'float'
+    ,   'bool'      : 'boolean'
+    ,   'datetime64[ns, UTC]'   :   'datetime'
+}
+
+
+def get_column_types(pd_data_types):
+    sql_data_types = []
+
+    for c in pd_data_types:
+        sql_data_types.append(DATA_TYPE_MAPPING.get(str(c), "varchar"))
+
+    return sql_data_types
 
 class SqlDb(object):
     def __init__(self, server, db_name):
@@ -6,14 +25,33 @@ class SqlDb(object):
                     "Server={};"
                     "Database={};"
                     "Trusted_Connection=yes".format(server, db_name))
+        self.database = db_name
         self.conn = pyodbc.connect(conn_str)
 
     def __del__(self):
         self.conn.close()
 
-    def query(self, sql):
+    def query(self, sql, read_query = False):
         with self.conn.cursor() as cursor:
             cursor.execute(sql)
             self.conn.commit()
-            return cursor.fetchall()
+            if read_query: return cursor.fetchall()
+
+    def create_table(self, table_name, dataframe):
+
+        self.query("USE {} DROP TABLE IF EXISTS {}".format(self.database, table_name))
+        
+        # Get column names and types
+        column_names = list(dataframe.columns.values)
+        column_types = get_column_types(dataframe.dtypes)
+        
+        # Create new table
+        create_table_query = "USE " + self.database + " CREATE TABLE " + table_name + " ("
+        for i in range(len(column_names)):
+            create_table_query = create_table_query + '\n' + column_names[i].replace(" ", "_") + ' ' + column_types[i] + ','
+        create_table_query = create_table_query[:-1] + " );"
+
+        return self.query(create_table_query)
+
+
 
